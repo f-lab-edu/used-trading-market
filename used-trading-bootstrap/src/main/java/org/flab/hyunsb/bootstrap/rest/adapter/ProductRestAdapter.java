e169b2ec6ccd727c5a277a7a4b2efe6320d64a9f
hyunsb <junghss0113@gmail.com>
2024-02-14 13:04:05 +0900

    Refactor: ResponseEntity 객체 생성 로직을 어노테이션을 통해 처리하도록 변경

--- used-trading-bootstrap/src/main/java/org/flab/hyunsb/bootstrap/rest/adapter/ProductRestAdapter.java
+++ used-trading-bootstrap/src/main/java/org/flab/hyunsb/bootstrap/rest/adapter/ProductRestAdapter.java
@@ -6,14 +6,14 @@ import org.flab.hyunsb.application.usecase.CreateProductUseCase;
 import org.flab.hyunsb.application.usecase.GetProductUseCase;
 import org.flab.hyunsb.bootstrap.rest.dto.ProductCreateRequest;
 import org.flab.hyunsb.bootstrap.rest.dto.ProductCreateResponse;
-import org.flab.hyunsb.bootstrap.rest.dto.ProductQueryResponse;
+import org.flab.hyunsb.bootstrap.rest.dto.ProductFindResponse;
 import org.flab.hyunsb.domain.Product;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -24,21 +24,21 @@ public class ProductRestAdapter {
     private final GetProductUseCase getProductUseCase;
 
     @PostMapping("/products")
-    public ResponseEntity<ProductCreateResponse> createProduct(
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductCreateResponse createProduct(
         @RequestBody @Valid ProductCreateRequest productCreateRequest) {
 
         Product product = productCreateRequest.toEntity();
         product = createProductUseCase.createProduct(product);
-        ProductCreateResponse response = ProductCreateResponse.from(product);
 
-        return new ResponseEntity<>(response, HttpStatus.CREATED);
+        return  ProductCreateResponse.from(product);
     }
 
     @GetMapping("/products/{id}")
-    public ResponseEntity<Object> getProduct(@PathVariable long id) {
+    @ResponseStatus(HttpStatus.OK)
+    public ProductFindResponse getProduct(@PathVariable long id) {
         Product product = getProductUseCase.getProductById(id);
-        ProductQueryResponse response = ProductQueryResponse.from(product);
 
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        return ProductFindResponse.from(product);
     }
 }
