13b6dd3e745a6df53e13cee492552537e8eeac00
hyunsb <junghss0113@gmail.com>
2024-03-05 13:33:32 +0900

    Feature: 시간 값 자동 주입을 위한 Auditing 설정 추가

--- used-trading-framework/src/main/java/org/flab/hyunsb/framework/persistence/config/PersistenceConfig.java
+++ used-trading-framework/src/main/java/org/flab/hyunsb/framework/persistence/config/PersistenceConfig.java
@@ -3,9 +3,11 @@ package org.flab.hyunsb.framework.persistence.config;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
+@EnableJpaAuditing
 @EnableJpaRepositories(basePackages = "org.flab.hyunsb.framework.persistence.repository")
 @EntityScan(basePackages = "org.flab.hyunsb.framework.persistence.entity")
 @ComponentScan(basePackages = "org.flab.hyunsb.framework.persistence")
